4bbf194b6c5a2be8066afc44c34c9c1596b8ba9f
Brandon Cooper <cooperjbrandon@gmail.com>
2020-08-24 11:16:33 -0700

    add urls and fix permissions import

--- collab/urls.py
+++ collab/urls.py
@@ -13,9 +13,45 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
+from inspect import isclass
 
+import debug_toolbar
+from django.conf import settings
+from django.contrib import admin
+from django.urls import include, path
+from django.views.generic import TemplateView
+from dynamic_rest.routers import DynamicRouter
+
+from collab_app import views
+
+
+# auto-register views
+drest_router = DynamicRouter()
+
+for name in dir(views):
+    view = getattr(views, name)
+    if isclass(view) and getattr(view, 'serializer_class', None):
+        drest_router.register_resource(view, namespace='api')
+
+rest_framework_auth_urls = include('rest_framework.urls', namespace='rest_framework')
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', include(drest_router.urls)),
+    path('admin/', admin.site.urls),  # admin site
+    path('api-auth/', rest_framework_auth_urls),  # direct login through browseable api
 ]
+
+# rest-auth urls
+urlpatterns += [
+    path('rest-auth/', include('dj_rest_auth.urls')),  # django-rest-auth library
+    path('rest-auth/registration/', include('dj_rest_auth.registration.urls')),  # registration
+    # required so password reset works with djang-rest-auth TODO: DO I NEED THIS?
+    path('', include('django.contrib.auth.urls')),
+    # so allauth uses `verify-email` url in it's signup email.
+    # (see allauth/account/adapter.py `get_email_confirmation_url` method)
+    path('verify-email/<key>', TemplateView.as_view(), name='account_confirm_email')
+]
+
+if settings.DEBUG:
+    urlpatterns += [
+        path('__debug__/', include(debug_toolbar.urls)),
+    ]
---
+++ collab_app/permissions/__init__.py
@@ -0,0 +1,6 @@
+from collab_app.permissions.object_level import GateKeeper, SideGateKeeper
+
+# for flake8
+__all__ = [
+    'GateKeeper', 'SideGateKeeper'
+]
